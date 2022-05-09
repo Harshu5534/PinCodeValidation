db47960164a303b3ccee86d365978ae7c8140087
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-09 13:59:12 +0530

    [Harshal]Add:Using Regular Expression

---
+++ RegixValidation/PinCodeValidation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegixValidationDemo
+{
+    public class PinCodeValidation
+    {
+        public const string PINCODE_REGEX = "^[0-9]{6}$";
+        //public const string EMAIL_REGEX = "^[a-z]$";
+        //public const string EMAIL_REGEX = "^[a-z]+[@]{1}[a-z]$";
+        //public const string EMAIL_REGEX = "^[a-z]+[@][a-z]+[.][a-z]{2,3}([.][a-z]{2}){0,1}$";
+        public static string EMAIL_REGEX = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
+        public void Validate(string pincode)
+        {
+            if (Regex.IsMatch(pincode, PINCODE_REGEX))
+            {
+                Console.WriteLine("Pincode Matches");
+            }
+            else
+            {
+                Console.WriteLine("Verify Pincode Again");
+            }
+        }
+        public void ValidateEmail(string email)
+        {
+            if (Regex.IsMatch(email,EMAIL_REGEX))
+            {
+                Console.WriteLine("Email Matches");
+            }
+            else
+            {
+                Console.WriteLine("Verify Email Again");
+            }
+        }
+    }
+}
---
+++ RegixValidation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace RegixValidationDemo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            PinCodeValidation validation = new PinCodeValidation();
+            validation.Validate("425404");
+            //validation.Validate("A42540");
+            //validation.Validate("42540B");
+            //validation.Validate("425 404");
+            //validation.ValidateEmail("b");
+            //validation.ValidateEmail("abc@gmail.com.in");
+            validation.ValidateEmail("harshalapatil2796@gmail.com");
+        }
+    }
+}
